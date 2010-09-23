bb8c7680bc365a8b9bd4854b4c6927e28eef7bd5
bendlas <hhochleitner@gmail.com>
2010-09-23 21:33:48 +0200

    init checkin

---
+++ src/at/elektroschock/eyefood/FoodView.java
@@ -0,0 +1,13 @@
+package at.elektroschock.eyefood;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FoodView extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
