ccac41fb13c61a5a023dc7d0730b54392b6faac1
serprime <philipp.a.steinwender@gmail.com>
2010-10-01 16:09:16 +0200

    bla

--- src/at/elektroschock/eyefood/FoodView.java
+++ src/at/elektroschock/eyefood/FoodView.java
@@ -1,7 +1,9 @@
 package at.elektroschock.eyefood;
 
 import android.app.Activity;
+import android.content.*;
 import android.os.Bundle;
+import android.view.*;
 
 public class FoodView extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,22 @@ public class FoodView extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.food_menu, menu);
+    	return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch(item.getItemId()) {
+    	case R.id.upload:
+    		Intent intent = new Intent(this, UploadView.class);
+    		startActivity(intent);
+    	}
+    	return true;
     }
 }
